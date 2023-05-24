f97869e946ac4a48ad4a27574e6850e5402e7bc5
karkolomik <karkolom@gmail.com>
2023-05-24 20:55:02 +0300

    bg-title img upgraded

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import MainButton from "../MainButton";
+import BgLogo from "../../images/who-we-are/title-bg2.png";
 
 function Hero() {
   return (
     <>
       <section className="hero-section w-full h-screen min800:flex min:800-justify-center min800:text-center">
         <div className="container-hero">
-          <div className="flex flex-col text-white absolute hero-text min800:items-center min800:-right-[22rem] min620:-roght-[20rem] min800:w-[80%] min540:-right-[17rem] min450:-right-[14rem] min375:-right-[11rem]">
-            <p className="text-black bg-white drop-shadow-2xl text-[24px] uppercase font-medium absolute ml-20 z-10 min800:pl-0 ">
+          <div
+            className="flex flex-col text-white
+           absolute hero-text min800:items-center min800:-right-[22rem] min620:-right-[20rem] min800:w-[80%] min540:-right-[17rem] min450:-right-[14rem] min375:-right-[11rem]"
+          >
+            <p className="text-black text-[20px] uppercase font-medium relative z-10 pl-16 m-8 mr-8 min800:pl-0 mb-8">
               &nbsp; it's time to &nbsp;
             </p>
-            {/* <img src="" alt="" /> */}
+            <img
+              src={BgLogo}
+              alt="s"
+              className=" w-[44%] absolute -top-10 left-8 title-white min800:w-[24rem] "
+            />
             <h1 className="font-bold text-[48px] uppercase mb-[4rem] min450:text-[40px] mt-20">
               &nbsp;&nbsp;dedicate yourself <br /> towards fitness
             </h1>
